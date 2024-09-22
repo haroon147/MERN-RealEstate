558b3f716993b7e848a1798f1e959de25a45ebf7
haroon147honey <147haroon@gmail.com>
2024-09-22 16:46:04 +0500

    Connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("we are Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://147haroon:honey147@mern-realestate.y01tr.mongodb.net/MERN-RealEstate?retryWrites=true&w=majority&appName=MERN-RealEstate"
+//   )
+//   .then(() => {
+//     console.log("connected too DB");
+//   });
 const app = express();
 
 app.listen(3000, () => {
