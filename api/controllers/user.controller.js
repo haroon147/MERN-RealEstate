77d39493e3505e3aaf085414c2f4e5561dcb4c6c
haroon147honey <147haroon@gmail.com>
2024-09-24 11:57:51 +0500

    solve all the errors and sended response to the server using insomania

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 export const test = (req, res) => {
   res.json({
-    message: "It is comming from controller",
+    message: "i am inside json",
   });
 };
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -22,7 +23,11 @@ mongoose
 //   });
 const app = express();
 
-app.use("/api/user", userRouter);
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running at:3000");
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
