b46ef0e849d8a6e739630ecaf8d8be57f6e9a381
haroon147honey <147haroon@gmail.com>
2024-09-24 14:24:06 +0500

    solved all the errors

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -2,21 +2,11 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     username: {
-<<<<<<< HEAD
-      type: String,
-      unique: true,
-      // unique: [true, "username is unique"],
-    },
-    email: {
-      // unique: [true, "email is Unique"],
-    },
-=======
       unique: true,
       type: String,
       require: true,
     },
     email: {},
->>>>>>> 9e287e69b9573269edd8086907f4de78203c096d
     password: {},
   },
   { timestamps: true }
